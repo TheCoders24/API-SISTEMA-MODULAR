3990be5d5ffc9ffe5d2247c742fa0ce14f5239b2
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2025-05-22 20:46:26 -0600

    add correction

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\LENOVO L14\Desktop\API-SISTEMA-MODULAR\.venv
+set VIRTUAL_ENV=C:\Users\ALEXIS\Desktop\API-SISTEMA-MODULAR\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
